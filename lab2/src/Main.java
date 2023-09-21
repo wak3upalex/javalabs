5d692c8c6c73de20dbabde11a1b565fd5b4ccaa4
ad230 <aleshadmi@mail.ru>
2023-09-21 11:20:57 +0300

    Main done. Need to fix errors

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,5 +1,23 @@
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AnnotatedMethods obj = new AnnotatedMethods(); //создаем объект класса AnnotatedMethods
+        Method[] methods = AnnotatedMethods.class.getDeclaredMethods(); //получаем массив методов класса AnnotatedMethods
+
+        for (Method method : methods) { //проходимся по всем методам класса AnnotatedMethods
+            if (method.isAnnotationPresent(Repeat.class)) { //если метод помечен аннотацией Repeat
+                int repeatCount = method.getAnnotation(Repeat.class).value(); //получаем значение аннотации Repeat
+                method.setAccessible(true); //устанавливаем флаг доступа к методу
+                for (int i = 0; i < repeatCount; i++) { //проходимся по циклу в соответствии с полученным значением аннотации
+                    try {
+                        method.invoke(obj); //вызываем метод
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
     }
 }
